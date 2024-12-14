5d70bc5e27a0a0eaaf6b8ce1f25de86b8dffdd8f
Fabriciogg8 <fgonzalezguasque@gmail.com>
2024-12-14 15:35:37 -0300

    Adding Group to the logic

--- main.py
+++ main.py
@@ -12,16 +12,25 @@ def main():
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     
-    x = SCREEN_WIDTH / 2
-    y = SCREEN_HEIGHT / 2
-    player = Player(x,y,PLAYER_RADIUS)
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    
+    player_init_x = SCREEN_WIDTH / 2
+    player_init_y = SCREEN_HEIGHT / 2
+    player = Player(player_init_x,player_init_y,PLAYER_RADIUS)
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(color="black")
-        player.update(dt)
-        player.draw(screen)
+        
+        for object in updatable:
+            object.update(dt)
+        for object in drawable:
+            object.draw(screen)
+        
         pygame.display.flip()
         delta_time = clock.tick(60) 
         dt = delta_time/1000
